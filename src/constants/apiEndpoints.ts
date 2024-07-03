29ed08ef889ae7d7bc51b06f703684347d8786a4
Lionel Bui <127284987+lionelbui1992@users.noreply.github.com>
2024-07-03 14:01:39 +0700

    Update apiEndpoints.ts

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -1,4 +1,4 @@
-export const OPENAI_ENDPOINT = 'http://127.0.0.1:5000/api';
+export const OPENAI_ENDPOINT = 'http://47.129.6.189:5000/api';
 export const TTS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/audio/speech`;
 export const CHAT_COMPLETIONS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/chat/completions`;
 export const MODELS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/models`;
