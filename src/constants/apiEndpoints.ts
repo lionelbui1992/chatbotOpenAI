05d41c54cfd6cfad18b1f4840e5044bf268179c2
donglv <lodong601@gmail.com>
2024-07-12 14:02:04 +0700

    hide side bar after logedout

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -1,6 +1,6 @@
 // export const OPENAI_ENDPOINT = 'http://127.0.0.1:5000/api';
 export const OPENAI_ENDPOINT = 'https://ec2-47-129-6-189.ap-southeast-1.compute.amazonaws.com/api';
-export const API_ENDPOINT = 'https://ec2-47-129-6-189.ap-southeast-1.compute.amazonaws.com/api';
+export const API_ENDPOINT = 'https://99ba-118-70-184-26.ngrok-free.app/api';
 export const AUTH_ENDPOINT = `${OPENAI_ENDPOINT}/v1/auth`;
 export const USER_ENDPOINT = `${OPENAI_ENDPOINT}/v1/user`;
 export const TTS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/audio/speech`;
