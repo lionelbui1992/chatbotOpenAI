9aedf0ff82c6a7de2261b152f60d30f953ef703f
donglv <lodong601@gmail.com>
2024-07-12 14:26:52 +0700

    update hide side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n';
@@ -11,10 +11,11 @@ import CustomChatEditor from './components/CustomChatEditor';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
 import Register from './components/Register';
+import { UserContext } from './UserContext';
 
 const App = () => {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
-
+  const {userSettings, setUserSettings} = useContext(UserContext);
   const toggleSidebarCollapse = () => {
     setIsSidebarCollapsed(!isSidebarCollapsed);
   };
@@ -39,11 +40,16 @@ const App = () => {
           <div className="App dark:bg-gray-900 dark:text-gray-100">
             <ToastContainer/>
             <div className="flex overflow-hidden w-full h-full relative z-0">
-              <Sidebar
+              {
+                userSettings.token ? (
+                  <Sidebar
                   className="sidebar-container flex-shrink-0"
                   isSidebarCollapsed={isSidebarCollapsed}
-                  toggleSidebarCollapse={toggleSidebarCollapse}
-              />
+                  toggleSidebarCollapse={toggleSidebarCollapse}/>
+                ):(
+                  <></>
+                )
+              }
               <div className="flex-grow h-full overflow-hidden">
                 <Routes>
                   <Route path="/" element={<ProtectedRoute><MainPageWithProps/></ProtectedRoute>}/>
