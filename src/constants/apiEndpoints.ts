390dc471eeaa9937a13ba57613de3a1235dcc42a
luong mon <ducmonmz@gmail.com>
2024-07-26 08:36:53 +0700

    change api to use ngrok

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -1,8 +1,8 @@
-// export const OPENAI_ENDPOINT = 'http://127.0.0.1:5000/api';
-export const OPENAI_ENDPOINT = 'https://ec2-47-129-6-189.ap-southeast-1.compute.amazonaws.com/api';
-export const API_ENDPOINT = 'https://99ba-118-70-184-26.ngrok-free.app/api';
-export const AUTH_ENDPOINT = `${OPENAI_ENDPOINT}/v1/auth`;
-export const USER_ENDPOINT = `${OPENAI_ENDPOINT}/v1/user`;
-export const TTS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/audio/speech`;
-export const CHAT_COMPLETIONS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/chat/completions`;
-export const MODELS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/models`;
+// export const API_ENDPOINT = 'http://127.0.0.1:5000/api';
+// export const API_ENDPOINT = 'https://ec2-47-129-6-189.ap-southeast-1.compute.amazonaws.com/api';
+export const API_ENDPOINT = 'https://ff33-118-70-184-26.ngrok-free.app/api';
+export const AUTH_ENDPOINT = `${API_ENDPOINT}/v1/auth`;
+export const USER_ENDPOINT = `${API_ENDPOINT}/v1/user`;
+export const TTS_ENDPOINT = `${API_ENDPOINT}/v1/audio/speech`;
+export const CHAT_COMPLETIONS_ENDPOINT = `${API_ENDPOINT}/v1/chat/completions`;
+export const MODELS_ENDPOINT = `${API_ENDPOINT}/v1/models`;
