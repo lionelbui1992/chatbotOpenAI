148f59ca611719bea2d3444f745edfe866d42588
luong mon <ducmonmz@gmail.com>
2024-08-02 10:36:59 +0700

    update api url

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -1,6 +1,6 @@
 // export const API_ENDPOINT = 'http://127.0.0.1:5000/api';
 // export const API_ENDPOINT = 'https://ec2-47-129-6-189.ap-southeast-1.compute.amazonaws.com/api';
-export const API_ENDPOINT = 'https://anhmok.com/ai/api';
+export const API_ENDPOINT = 'https://ai.anhmok.com/api';
 export const AUTH_ENDPOINT = `${API_ENDPOINT}/v1/auth`;
 export const USER_ENDPOINT = `${API_ENDPOINT}/v1/user`;
 export const TTS_ENDPOINT = `${API_ENDPOINT}/v1/audio/speech`;
